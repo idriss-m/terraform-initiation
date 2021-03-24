a409a79c2869f558326fbd6a9c6bd458de06ab3f
idriss <idriss.mezguich@esprit.tn>
2021-03-24 22:41:54 +0100

    add terraform files

---
+++ az-remote-backend-variables.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+  type        = string
+  description = "This variable defines the prefix"
+}
+# environment
+variable "environment" {
+  type        = string
+  description = "This variable defines the environment to be built"
+}
+# azure region
+variable "location" {
+  type        = string
+  description = "Azure region where the resource group will be created"
+  default     = "north europe"
+}
